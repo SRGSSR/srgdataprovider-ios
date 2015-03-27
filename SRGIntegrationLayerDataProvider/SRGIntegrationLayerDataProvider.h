76538b61e40cf0f8e4cf183eade702271b2ece0d
Cédric Foellmi <cedric@onekilopars.ec>
2015-03-27 16:00:34 +0100

    Initial import

---
+++ SRGIntegrationLayerDataProvider/SRGIntegrationLayerDataProvider.h
@@ -0,0 +1,13 @@
+//
+//  SRGIntegrationLayerDataProvider.h
+//  SRGIntegrationLayerDataProvider
+//
+//  Created by Cédric Foellmi on 27/03/15.
+//  Copyright (c) 2015 SRG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SRGIntegrationLayerDataProvider : NSObject
+
+@end
---
+++ SRGIntegrationLayerDataProvider/SRGIntegrationLayerDataProvider.m
@@ -0,0 +1,13 @@
+//
+//  SRGIntegrationLayerDataProvider.m
+//  SRGIntegrationLayerDataProvider
+//
+//  Created by Cédric Foellmi on 27/03/15.
+//  Copyright (c) 2015 SRG. All rights reserved.
+//
+
+#import "SRGIntegrationLayerDataProvider.h"
+
+@implementation SRGIntegrationLayerDataProvider
+
+@end
