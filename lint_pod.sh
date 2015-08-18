dc6211c1329788698c3daffd8007c4d062e71922
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-18 14:30:38 +0200

    Fixed lint_pod script

--- lint_pod.sh
+++ lint_pod.sh
@@ -1,2 +1,2 @@
-pod lib lint SRGIntegrationLayerDataProvider.podspec --allow-warnings
+pod lib lint SRGIntegrationLayerDataProvider.podspec --allow-warnings --sources="ssh://git@bitbucket.org/rtsmb/srgpodspecs.git,https://github.com/CocoaPods/Specs.git"
 
