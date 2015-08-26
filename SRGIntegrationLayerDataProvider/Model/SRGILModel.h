6b3f8a3bb3fa65a1188d324973d1e45beb409693
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-26 16:27:23 +0200

    Adding missing header

--- SRGIntegrationLayerDataProvider/Model/SRGILModel.h
+++ SRGIntegrationLayerDataProvider/Model/SRGILModel.h
@@ -16,6 +16,7 @@
 #import "SRGILAssetMetadata.h"
 #import "SRGILAssetSet.h"
 #import "SRGILAudio.h"
+#import "SRGILDownload.h"
 #import "SRGILImage.h"
 #import "SRGILImageRepresentation.h"
 #import "SRGILLiveHeaderChannel.h"
