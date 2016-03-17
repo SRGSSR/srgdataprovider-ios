b7788e7c093fe6e2076f4e16bb0fee2d52120bb7
Samuel Défago <defagos@gmail.com>
2016-03-17 09:00:44 +0100

    Parse additional colors available from the event service

--- SRGIntegrationLayerDataProvider/Model/SRGILEventConfig.h
+++ SRGIntegrationLayerDataProvider/Model/SRGILEventConfig.h
@@ -13,8 +13,12 @@
 @property (nonatomic, readonly) NSString *title;
 
 @property (nonatomic, readonly) UIColor *backgroundColor;
+
 @property (nonatomic, readonly) UIColor *textColor;
 @property (nonatomic, readonly) UIColor *linkColor;
+
+@property (nonatomic, readonly) UIColor *headerBackgroundColor;
+@property (nonatomic, readonly) UIColor *headerTitleColor;
 
 @property (nonatomic, readonly) NSURL *backgroundImageURL;
 @property (nonatomic, readonly) NSURL *logoImageURL;
