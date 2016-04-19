8417bdfdb8edb360e2ab82e3a0f76fb8d6c3ffeb
Samuel Défago <defagos@gmail.com>
2016-04-19 07:59:24 +0200

    Update development pods

---
+++ Pods/Headers/Private/SRGIntegrationLayerDataProvider/SRGILDataProviderAnalyticsDataSource.h
@@ -0,0 +1,1 @@
+../../../../SRGIntegrationLayerDataProvider/MediaPlayer/SRGILDataProviderAnalyticsDataSource.h
--- Pods/Headers/Public/SRGIntegrationLayerDataProvider/SRGILDataProviderMediaPlayerDataSource.h
+++
@@ -1,1 +0,0 @@
-../../../../SRGIntegrationLayerDataProvider/SRGILDataProviderMediaPlayerDataSource.h
