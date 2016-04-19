26835127f7dad704f85469f93f6fd5d61409c6b4
Samuel Défago <defagos@gmail.com>
2016-04-19 16:00:33 +0200

    Enable no clean in case of validation error

--- lint_pod.sh
+++ lint_pod.sh
@@ -1,2 +1,2 @@
-pod lib lint SRGIntegrationLayerDataProvider.podspec --verbose --allow-warnings --use-libraries --sources="ssh://git@bitbucket.org/rtsmb/srgpodspecs.git,https://github.com/CocoaPods/Specs.git"
+pod lib lint SRGIntegrationLayerDataProvider.podspec --verbose --no-clean --allow-warnings --use-libraries --sources="ssh://git@bitbucket.org/rtsmb/srgpodspecs.git,https://github.com/CocoaPods/Specs.git"
 
