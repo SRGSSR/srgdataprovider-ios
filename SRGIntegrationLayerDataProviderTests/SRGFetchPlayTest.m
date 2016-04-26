c15ee7cb85cbc446bd6a578675817b0ba174c93a
Pierre-Yves Bertholon <pierre-yves.bertholon@hortis.ch>
2016-04-26 15:56:23 +0200

    Add fetch test on video/play and audio/play

--- SRGIntegrationLayerDataProviderTests/SRGFetchPlayTest.m
+++ SRGIntegrationLayerDataProviderTests/SRGFetchPlayTest.m
@@ -13,6 +13,8 @@
 #import "SRGILVideo.h"
 #import "SRGILAudio.h"
 
+static NSString *const SRFVideoURN = @"urn:srf:video:3b5ff803-6f32-4421-a736-d10ce35bf4b0";
+static NSString *const SRFAudioURN = @"urn:srf:audio:e7cfd700-e14e-43b4-9710-3527fc2098bc";
 static NSString *const SRFVideoSetURN = @"urn:srf:videoSet:91035074-f90e-472c-9bce-66bc3f114ff0";
 
 
@@ -35,14 +37,31 @@ static NSString *const SRFVideoSetURN = @"urn:srf:videoSet:91035074-f90e-472c-9b
 {
     XCTestExpectation *expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
     
-    SRGILURN *videoSetURN = [SRGILURN URNWithString:SRFVideoSetURN];
+    SRGILURN *videoURN = [SRGILURN URNWithString:SRFVideoURN];
     
-    [self.dataProvider fetchMediaWithURN:videoSetURN
+    [self.dataProvider fetchMediaWithURN:videoURN
                          completionBlock:^(id  _Nullable media, NSError * _Nullable error) {
                              [expectation fulfill];
                              XCTAssertNil(error);
                              XCTAssertNotNil(media);
                              XCTAssertTrue([media isKindOfClass:[SRGILVideo class]]);
+                         }];
+    
+    [self waitForExpectationsWithTimeout:30.0 handler:nil];
+}
+
+- (void)testFetchAudio
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+    
+    SRGILURN *audioURN = [SRGILURN URNWithString:SRFAudioURN];
+    
+    [self.dataProvider fetchMediaWithURN:audioURN
+                         completionBlock:^(id  _Nullable media, NSError * _Nullable error) {
+                             [expectation fulfill];
+                             XCTAssertNil(error);
+                             XCTAssertNotNil(media);
+                             XCTAssertTrue([media isKindOfClass:[SRGILAudio class]]);
                          }];
     
     [self waitForExpectationsWithTimeout:30.0 handler:nil];
