4cede38f4cc2f1c985b93a5788132e5a546c8a67
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2016-06-14 15:03:49 +0200

    Get HLS DVR URLs for videos, if we have it

--- SRGIntegrationLayerDataProvider/Model/SRGILVideo.m
+++ SRGIntegrationLayerDataProvider/Model/SRGILVideo.m
@@ -21,19 +21,23 @@
     BOOL takeHDVideo = [[NSUserDefaults standardUserDefaults] boolForKey:SRGILVideoUseHighQualityOverCellularNetworkKey];
     BOOL usingTrueWIFINetwork = [SRGILRequestsManager isUsingWIFI] && ![SRGILRequestsManager isUsingSwisscomWIFI];
     
+    // HLS DVR first, HLS otherwise
+    NSURL *qualityHDURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLSDVR withQuality:SRGILPlaylistURLQualityHD];
+    if (!qualityHDURL) {
+        qualityHDURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualityHD];
+    }
+    NSURL *qualitySDURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLSDVR withQuality:SRGILPlaylistURLQualitySD];
+    if (!qualitySDURL) {
+        qualitySDURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualitySD];
+    }
+    
     if (usingTrueWIFINetwork || takeHDVideo) {
         // We are on True WIFI (non-Swisscom) or the HD quality switch is ON. We play HD (HLS) if we have it. SD otherwise.
-        contentURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualityHD];
-        if (!contentURL) {
-            contentURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualitySD];
-        }
+        contentURL = qualityHDURL ?: qualitySDURL;
     }
     else {
         // We are not on WIFI and switch is OFF. YES, business decision: we play HD as backup if we don't have SD.
-        contentURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualitySD];
-        if (!contentURL) {
-            contentURL = [self contentURLForPlaylistWithProtocol:SRGILPlaylistProtocolHLS withQuality:SRGILPlaylistURLQualityHD];
-        }
+        contentURL = qualitySDURL ?: qualityHDURL;
     }
     
     return contentURL;
