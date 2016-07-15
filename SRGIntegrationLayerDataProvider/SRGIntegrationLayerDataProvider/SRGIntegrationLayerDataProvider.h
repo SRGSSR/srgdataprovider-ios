1e1b580132b6cb7eb63c60c65013947f00cad5e3
Samuel Défago <defagos@gmail.com>
2016-07-15 08:25:25 +0200

    Add framework project

---
+++ SRGIntegrationLayerDataProvider/SRGIntegrationLayerDataProvider/SRGIntegrationLayerDataProvider.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <UIKit/UIKit.h>
+
+// Project version number
+FOUNDATION_EXPORT double SRGIntegrationLayerDataProviderVersionNumber;
+
+// Project version string
+FOUNDATION_EXPORT const unsigned char SRGIntegrationLayerDataProviderVersionString[];
+
+// Public headers
