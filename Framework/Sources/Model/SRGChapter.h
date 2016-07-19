cef91a42ccc58a560749eb2d689c7914b3c887da
Samuel Défago <defagos@gmail.com>
2016-07-19 14:59:40 +0200

    Do not make chapter inherit from media
    Jörg Brokamp: They share a lot of fields but are semantically complete different

--- Framework/Sources/Model/SRGChapter.h
+++ Framework/Sources/Model/SRGChapter.h
@@ -4,12 +4,33 @@
 //  License information is available from the LICENSE file.
 //
 
-#import "SRGMedia.h"
 #import "SRGResource.h"
+#import "SRGTypes.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SRGChapter : SRGMedia
+@interface SRGChapter : MTLModel <MTLJSONSerializing>
+
+@property (nonatomic, readonly, copy) NSString *uid;
+
+@property (nonatomic, readonly, copy) NSString *URN;
+@property (nonatomic, readonly, copy) NSString *vendor;
+@property (nonatomic, readonly) SRGMediaType mediaType;
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy) NSString *lead;
+@property (nonatomic, readonly, copy) NSString *summary;
+
+@property (nonatomic, readonly) NSURL *imageURL;
+@property (nonatomic, readonly, copy) NSString *imageTitle;
+
+@property (nonatomic, readonly) SRGContentType contentType;
+@property (nonatomic, readonly) SRGSource source;
+
+@property (nonatomic, readonly) NSDate *date;
+@property (nonatomic, readonly) NSTimeInterval duration;
+
+@property (nonatomic, readonly) NSURL *podcastStandardDefinitionURL;
+@property (nonatomic, readonly) NSURL *podcastHighDefinitionURL;
 
 @property (nonatomic, readonly) NSInteger position;
 @property (nonatomic, readonly) NSTimeInterval markIn;
--- Framework/Sources/Model/SRGChapter.m
+++ Framework/Sources/Model/SRGChapter.m
@@ -6,7 +6,30 @@
 
 #import "SRGChapter.h"
 
+#import "SRGJSONTransformers.h"
+
 @interface SRGChapter ()
+
+@property (nonatomic, copy) NSString *uid;
+
+@property (nonatomic, copy) NSString *URN;
+@property (nonatomic, copy) NSString *vendor;
+@property (nonatomic) SRGMediaType mediaType;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *lead;
+@property (nonatomic, copy) NSString *summary;
+
+@property (nonatomic) NSURL *imageURL;
+@property (nonatomic, copy) NSString *imageTitle;
+
+@property (nonatomic) SRGContentType contentType;
+@property (nonatomic) SRGSource source;
+
+@property (nonatomic) NSDate *date;
+@property (nonatomic) NSTimeInterval duration;
+
+@property (nonatomic) NSURL *podcastStandardDefinitionURL;
+@property (nonatomic) NSURL *podcastHighDefinitionURL;
 
 @property (nonatomic) NSInteger position;
 @property (nonatomic) NSTimeInterval markIn;
@@ -25,17 +48,65 @@
     static NSDictionary *mapping;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSMutableDictionary *mutableMapping = [[super JSONKeyPathsByPropertyKey] mutableCopy];
-        [mutableMapping addEntriesFromDictionary:@{ @"position" : @"position",
-                                                    @"markIn" : @"markIn",
-                                                    @"markOut" : @"markOut",
-                                                    @"resources" : @"resourceList" }];
-        mapping = [mutableMapping copy];
+        mapping = @{ @"uid" : @"id",
+                     @"URN" : @"urn",
+                     @"vendor" : @"vendor",
+                     @"mediaType" : @"mediaType",
+                     @"title" : @"title",
+                     @"lead" : @"lead",
+                     @"summary" : @"description",
+                     @"imageURL" : @"imageUrl",
+                     @"imageTitle" : @"imageTitle",
+                     @"contentType" : @"type",
+                     @"source" : @"assignedBy",
+                     @"date" : @"date",
+                     @"duration" : @"duration",
+                     @"podcastStandardDefinitionURL" : @"podcastSdUrl",
+                     @"podcastHighDefinitionURL" : @"podcastHdUrl",
+                     @"position" : @"position",
+                     @"markIn" : @"markIn",
+                     @"markOut" : @"markOut",
+                     @"resources" : @"resourceList" };
     });
     return mapping;
 }
 
 #pragma mark Transformers
+
++ (NSValueTransformer *)mediaTypeJSONTransformer
+{
+    return SRGMediaTypeJSONTransformer();
+}
+
++ (NSValueTransformer *)imageURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)contentTypeJSONTransformer
+{
+    return SRGContentTypeJSONTransformer();
+}
+
++ (NSValueTransformer *)sourceJSONTransformer
+{
+    return SRGSourceJSONTransformer();
+}
+
++ (NSValueTransformer *)dateJSONTransformer
+{
+    return SRGISO8601DateJSONTransformer();
+}
+
++ (NSValueTransformer *)podcastStandardDefinitionURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
+
++ (NSValueTransformer *)podcastHighDefinitionURLJSONTransformer
+{
+    return [NSValueTransformer valueTransformerForName:MTLURLValueTransformerName];
+}
 
 + (NSValueTransformer *)resourcesJSONTransformer
 {
