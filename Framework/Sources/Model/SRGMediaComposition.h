83f5bae33fd54424291ae73375a00e33d2b74a0e
Samuel Défago <defagos@gmail.com>
2016-07-19 08:07:44 +0200

    Add missing nullability macros

--- Framework/Sources/Model/SRGMediaComposition.h
+++ Framework/Sources/Model/SRGMediaComposition.h
@@ -10,6 +10,8 @@
 #import "SRGEpisode.h"
 #import "SRGShow.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface SRGMediaComposition : MTLModel <MTLJSONSerializing>
 
 @property (nonatomic, readonly, copy) NSString *chapterURN;
@@ -21,3 +23,5 @@
 @property (nonatomic, readonly, copy) NSString *event;
 
 @end
+
+NS_ASSUME_NONNULL_END
