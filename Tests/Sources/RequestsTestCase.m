8fba6f132a2d41a9c4bfab9f6066c8006dd68da4
Samuel Défago <defagos@gmail.com>
2016-09-22 16:37:53 +0200

    Add test target

---
+++ Tests/Sources/RequestsTestCase.m
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RequestsTestCase : XCTestCase
+
+@end
+
+@implementation RequestsTestCase
+
+@end
