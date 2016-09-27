aa682ed3e4ff633e18363fbe55bf694ab5c8cf37
Samuel Défago <defagos@gmail.com>
2016-09-27 10:05:58 +0200

    Add documentation

--- Framework/Sources/SRGDataProvider.h
+++ Framework/Sources/SRGDataProvider.h
@@ -52,29 +52,95 @@ OBJC_EXPORT NSString * const SRGDataProviderBusinessUnitIdentifierRTS;
 OBJC_EXPORT NSString * const SRGDataProviderBusinessUnitIdentifierSRF;
 OBJC_EXPORT NSString * const SRGDataProviderBusinessUnitIdentifierSWI;
 
+// Completion block signatures
 typedef void (^SRGChannelListCompletionBlock)(NSArray<SRGChannel *> * _Nullable channels, NSError * _Nullable error);
 typedef void (^SRGEventListCompletionBlock)(NSArray<SRGEvent *> * _Nullable events, NSError * _Nullable error);
 typedef void (^SRGMediaListCompletionBlock)(NSArray<SRGMedia *> * _Nullable medias, SRGPage * _Nullable nextPage, NSError * _Nullable error);
 typedef void (^SRGShowListCompletionBlock)(NSArray<SRGShow *> * _Nullable shows, NSError * _Nullable error);
 typedef void (^SRGTopicListCompletionBlock)(NSArray<SRGTopic *> * _Nullable topics, NSError * _Nullable error);
 typedef void (^SRGURLCompletionBlock)(NSURL * _Nullable URL, NSError * _Nullable error);
-
 typedef void (^SRGLikeCompletionBlock)(SRGLike * _Nullable like, NSError * _Nullable error);
 typedef void (^SRGMediaCompositionCompletionBlock)(SRGMediaComposition * _Nullable mediaComposition, NSError * _Nullable error);
 
+/**
+ *  A data provider supplies metadata for an SRG SSR business unit (media and show lists, mostly). Several data providers 
+ *  can coexist in an application, though most application should only require one.
+ *
+ *  The data provider requests data from the Integration Layer SRG SSRG service, the service responsible of delivering
+ *  metadata common to all SRG SSR business units. To avoid unnecessary requests, the data provider relies on the standard
+ *  built-in iOS URL cache (`NSURLCache`).
+ *
+ *  ## Instantiation
+ * 
+ *  You instantiate a data provider with a service base URL and a business unit identifier. The service URL must expose
+ *  services whose endpoints start with 'integrationlayer/...', corresponding to a working Integration Layer installation.
+ *
+ *  If your application only requires data from a single business unit, you can use the data provider you need like a singleton
+ *  by instantiating it early in your application lifecycle (e.g. in your `-applicationDidFinishLaunching:withOptions:`
+ *  implementation) and setting it as global shared instance by calling `-[SRGDataProvider setCurrentDataProvider:]`.
+ *  You can then conveniently retrieve this shared instance with `-[SRGDataProvider currentDataProvider]`.
+ *
+ *  ## Requesting data
+ *
+ *  To request data, use the methods from the `Requests` category. These methods return an `SRGRequest` object, which
+ *  lets you manage the request process itself (starting or cancelling data retrieval). Requests expect a completion
+ *  block, which is called when the request finishes (either normally or with an error). The completion block will not
+ *  be called if the request is cancelled.
+ *
+ *  ## Page management
+ *
+ *  TODO: Write when we know whether all requests support page management or not
+ *
+ *  ## Connection queues
+ *
+ *
+ */
 @interface SRGDataProvider : NSObject <NSURLSessionTaskDelegate>
 
+/**
+ *  The data provider currently optionally set as shared instance, if any
+ *
+ *  @see `-setCurrentDataProvider:`
+ */
 + (nullable SRGDataProvider *)currentDataProvider;
+
+/**
+ *  Set a data provider as shared instance for convenient retrieval via `-currentDataProvider`.
+ *
+ *  @return The previously installed shared instance, if any
+ */
 + (nullable SRGDataProvider *)setCurrentDataProvider:(nullable SRGDataProvider *)currentDataProvider;
 
+/**
+ *  Instantiate a data provider
+ *
+ *  @param serviceURL             The Integration Layer service base URL (which must expose service endpoints
+ *                                starting with /integrationlayer)
+ *  @param businessUnitIdentifier The identifier of the SRG SSR business unit to retrieve data for. Use constants
+ *                                available at the top of this file for the officially supported values
+ */
 - (instancetype)initWithServiceURL:(NSURL *)serviceURL businessUnitIdentifier:(NSString *)businessUnitIdentifier NS_DESIGNATED_INITIALIZER;
 
-// Always ends with a slash
+/**
+ *  The service URL which has been set
+ *
+ *  @discussion Always ends with a slash
+ */
 @property (nonatomic, readonly) NSURL *serviceURL;
+
+/**
+ *  The business unit identifier which has been set
+ */
 @property (nonatomic, readonly, copy) NSString *businessUnitIdentifier;
 
-// TODO: Maybe have an audio / video enum parameter for each method available for audio & videos
+@end
+
+/**
+ *  List of the requests supported by the data provider
+ */
 // TODO: Document: completion block never called for cancelled requests
+// TODO: Maybe have an audio / video enum parameter for each method available for audio & videos
+@interface SRGDataProvider (Requests)
 
 - (SRGRequest *)editorialVideosWithCompletionBlock:(SRGMediaListCompletionBlock)completionBlock;
 
