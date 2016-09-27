8ea5144a618573229ba95e185272224a0745370c
Samuel Défago <defagos@gmail.com>
2016-09-27 16:15:32 +0200

    Add documentation

--- Framework/Sources/SRGRequest+Private.h
+++ Framework/Sources/SRGRequest+Private.h
@@ -8,10 +8,17 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// Block signatures
 typedef void (^SRGRequestCompletionBlock)(NSDictionary * _Nullable JSONDictionary, SRGPage * _Nullable nextPage, NSError * _Nullable error);
 
+/**
+ *  Private interface for implementation purposes
+ */
 @interface SRGRequest (Private)
 
+/**
+ *  Create a request from a URL request, starting it with the provided session, and calling the specified block on completion
+ */
 - (instancetype)initWithRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(SRGRequestCompletionBlock)completionBlock;
 
 @end
