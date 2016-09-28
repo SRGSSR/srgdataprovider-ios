617d2b0a4e180b2e40d54e2237218121b044a112
Samuel Défago <defagos@gmail.com>
2016-09-28 17:34:35 +0200

    Fix typo

--- Framework/Sources/Model/SRGLike.h
+++ Framework/Sources/Model/SRGLike.h
@@ -18,7 +18,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface SRGLike : MTLModel <SRGMediaIdentifierMetadata, MTLJSONSerializing>
 
 /**
- *  The update social media and popularity information
+ *  The updated social media and popularity information
  */
 @property (nonatomic, readonly, nullable) NSArray<SRGSocialCount *> *socialCounts;
 
