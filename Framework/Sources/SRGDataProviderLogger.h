e571aee65b830eaf7fb5b727d3efd7d6680741c0
Samuel Défago <defagos@gmail.com>
2016-09-30 17:36:08 +0200

    Fix typo

--- Framework/Sources/SRGDataProviderLogger.h
+++ Framework/Sources/SRGDataProviderLogger.h
@@ -9,8 +9,8 @@
 /**
  *  Helper macros for logging
  */
-#define SRGDataProviderLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssrg.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGDataProviderLogDebug(category, format, ...)   SRGLogDebug(@"ch.srgssrg.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGDataProviderLogInfo(category, format, ...)    SRGLogInfo(@"ch.srgssrg.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGDataProviderLogWarning(category, format, ...) SRGLogWarning(@"ch.srgssrg.dataprovider", category, format, ##__VA_ARGS__)
-#define SRGDataProviderLogError(category, format, ...)   SRGLogError(@"ch.srgssrg.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGDataProviderLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGDataProviderLogDebug(category, format, ...)   SRGLogDebug(@"ch.srgssr.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGDataProviderLogInfo(category, format, ...)    SRGLogInfo(@"ch.srgssr.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGDataProviderLogWarning(category, format, ...) SRGLogWarning(@"ch.srgssr.dataprovider", category, format, ##__VA_ARGS__)
+#define SRGDataProviderLogError(category, format, ...)   SRGLogError(@"ch.srgssr.dataprovider", category, format, ##__VA_ARGS__)
