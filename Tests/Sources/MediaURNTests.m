585f2ead2672697127c6fe86dbad65323a348d0e
Samuel Défago <defagos@gmail.com>
2017-01-20 11:37:54 +0100

    Add URN tests

--- Tests/Sources/MediaURNTests.m
+++ Tests/Sources/MediaURNTests.m
@@ -13,7 +13,7 @@
 
 @implementation MediaURNTests
 
-- (void)testSRGMediaURNForSWICreation
+- (void)testCreationSWI
 {
     NSString *URNString = @"urn:swi:video:41981254";
     
@@ -24,7 +24,7 @@
     XCTAssertEqualObjects(mediaURN.URNString, URNString);
 }
 
-- (void)testSRGMediaURNForSRFCreation
+- (void)testCreationSRF
 {
     NSString *URNString = @"urn:srf:ais:video:4a70cbd6-4575-43ce-a7ca-766f7e2fb974";
     
@@ -35,16 +35,27 @@
     XCTAssertEqualObjects(mediaURN.URNString, URNString);
 }
 
-- (void)testSRGFake1MediaURNCreation
+- (void)testIncorrectURNs
 {
-    SRGMediaURN *mediaURN = [[SRGMediaURN alloc] initWithURNString:@"fakeURN:swi:video:41981254"];
-    XCTAssertNil(mediaURN);
+    SRGMediaURN *mediaURN1 = [[SRGMediaURN alloc] initWithURNString:@"fakeURN:swi:video:41981254"];
+    XCTAssertNil(mediaURN1);
+    
+    SRGMediaURN *mediaURN2 = [[SRGMediaURN alloc] initWithURNString:@"swi:video:41981254"];
+    XCTAssertNil(mediaURN2);
 }
 
-- (void)testSRGFake2MediaURNCreation
+- (void)testEquality
 {
-    SRGMediaURN *mediaURN = [[SRGMediaURN alloc] initWithURNString:@"swi:video:41981254"];
-    XCTAssertNil(mediaURN);
+    SRGMediaURN *mediaURN1 = [[SRGMediaURN alloc] initWithURNString:@"urn:swi:video:41981254"];
+    SRGMediaURN *mediaURN2 = [[SRGMediaURN alloc] initWithURNString:@"urn:swi:video:41981254"];
+    SRGMediaURN *mediaURN3 = [[SRGMediaURN alloc] initWithURNString:@"urn:srf:video:41981254"];
+    SRGMediaURN *mediaURN4 = [[SRGMediaURN alloc] initWithURNString:@"urn:swi:video:99999999"];
+    SRGMediaURN *mediaURN5 = [[SRGMediaURN alloc] initWithURNString:@"urn:swi:audio:41981254"];
+    
+    XCTAssertTrue([mediaURN1 isEqual:mediaURN2]);
+    XCTAssertFalse([mediaURN1 isEqual:mediaURN3]);
+    XCTAssertFalse([mediaURN1 isEqual:mediaURN4]);
+    XCTAssertFalse([mediaURN1 isEqual:mediaURN5]);
 }
 
 @end
