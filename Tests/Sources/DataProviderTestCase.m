af345e3f9465a57e0f4df9b11316bb8a7aea3bdb
Samuel Défago <defagos@gmail.com>
2017-03-24 10:20:12 +0100

    Inhibit Clang warning

--- Tests/Sources/DataProviderTestCase.m
+++ Tests/Sources/DataProviderTestCase.m
@@ -36,8 +36,11 @@
 
 - (void)testDeallocation
 {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-unsafe-retained-assign"
     __weak SRGDataProvider *dataProvider = [[SRGDataProvider alloc] initWithServiceURL:SRGIntegrationLayerProductionServiceURL() businessUnitIdentifier:SRGDataProviderBusinessUnitIdentifierRTS];
     XCTAssertNil(dataProvider);
+#pragma clang diagnostic pop
 }
 
 @end
