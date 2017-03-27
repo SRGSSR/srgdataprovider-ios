3590ff8de94e61bd64700c805b54dc3d26ee1e07
Samuel Défago <defagos@gmail.com>
2017-03-27 13:07:19 +0200

    Remove now obvious behaviors
    Method renaming now makes clear that new requests are returned. Moreover, bad constructions are now detected at compile time.

--- Framework/Sources/SRGFirstPageRequest.h
+++ Framework/Sources/SRGFirstPageRequest.h
@@ -17,9 +17,6 @@ NS_ASSUME_NONNULL_BEGIN
  *  Return an equivalent request, but with the specified page size
  *
  *  @param pageSize The page size to use (values below 1 will be set to 1)
- *
- *  @discussion If `requestWithPageSize:`called twice or more, only the latest called value will be considered.
- *              `-requestWithPageSize:` can only on be called on the request for the first page.
  */
 - (SRGFirstPageRequest *)requestWithPageSize:(NSInteger)pageSize;
 
@@ -30,8 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
  *  @param page The page to request. If nil, the first page is requested (for the same page size as the receiver)
  *
  *  @discussion The `-requestWithPage:` method must be called on a related request, otherwise the behavior is undefined.
- *              When using a next page, You must NOT complete the request with the `requestWithPageSize:`method. PageSize is
- *              already known by the IL service, and it's the same as the page size in your first request.
  */
 - (SRGPageRequest *)requestWithPage:(nullable SRGPage *)page;
 
