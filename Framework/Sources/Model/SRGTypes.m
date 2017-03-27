ac756accb0404b2c6cb164a2c8b8a79fc47d9fdd
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2017-03-27 17:19:51 +0200

    Fix blocking reason unknown message

--- Framework/Sources/Model/SRGTypes.m
+++ Framework/Sources/Model/SRGTypes.m
@@ -20,7 +20,7 @@ NSString *SRGMessageForBlockingReason(SRGBlockingReason blockingReason)
                       @(SRGBlockingReasonAgeRating12) : SRGDataProviderLocalizedString(@"To protect children under the age of 12, this content is only available between 8 p.m. and 6 a.m.", nil),
                       @(SRGBlockingReasonStartDate) : SRGDataProviderLocalizedString(@"This content is not yet available. Please try again later.", nil),
                       @(SRGBlockingReasonEndDate) : SRGDataProviderLocalizedString(@"For legal reasons, this content was only available for a limited period of time.", nil),
-                      @(SRGBlockingReasonEndDate) : SRGDataProviderLocalizedString(@"This content is not available.", nil) };
+                      @(SRGBlockingReasonUnknown) : SRGDataProviderLocalizedString(@"This content is not available.", nil) };
     });
     return messages[@(blockingReason)];
 }
