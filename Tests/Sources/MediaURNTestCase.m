685e954ba0264136d767d9dccac0b6f228c78505
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2017-04-20 16:50:59 +0200

    Update tests for case sensitive and insensitive

--- Tests/Sources/MediaURNTestCase.m
+++ Tests/Sources/MediaURNTestCase.m
@@ -33,8 +33,22 @@
     SRGMediaURN *mediaURN2 = [[SRGMediaURN alloc] initWithURNString:@"urn:SWI:video:41981254"];
     XCTAssertNotNil(mediaURN2);
     
-    SRGMediaURN *mediaURN3 = [[SRGMediaURN alloc] initWithURNString:@"urn:swi:VIDEO:41981254"];
+    SRGMediaURN *mediaURN3 = [[SRGMediaURN alloc] initWithURNString:@"Urn:swi:VIDEO:41981254"];
     XCTAssertNotNil(mediaURN3);
+}
+
+- (void)testCaseSensitive
+{
+    SRGMediaURN *mediaURN1 = [[SRGMediaURN alloc] initWithURNString:@"urn:rsi:video:livestream_La1"];
+    XCTAssertNotNil(mediaURN1);
+    XCTAssertNotEqualObjects(mediaURN1.uid, @"livestream_la1");
+    XCTAssertEqualObjects(mediaURN1.uid, @"livestream_La1");
+    
+    SRGMediaURN *mediaURN2 = [[SRGMediaURN alloc] initWithURNString:@"urn:rtr:video:269e6a58-a9cb-11e3-ac2b-fbf4986f02ad"];
+    XCTAssertNotNil(mediaURN2);
+    XCTAssertNotEqualObjects(mediaURN2.uid, @"269E6A58-A9CB-11E3-AC2B-FBF4986F02AD");
+    XCTAssertNotEqualObjects(mediaURN2.uid, @"269e6a58-a9cb-11e3-ac2b-fbf4986f02AD");
+    XCTAssertEqualObjects(mediaURN2.uid, @"269e6a58-a9cb-11e3-ac2b-fbf4986f02ad");
 }
 
 - (void)testIncorrectURNs
