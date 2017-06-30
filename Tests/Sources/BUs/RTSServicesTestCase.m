78d3cc8a498be5463a00377e4c91abac19221600
Samuel Défago <defagos@gmail.com>
2017-06-30 15:05:02 +0200

    Update service availability
    The RTS by ids services will be soon available, see https://srfmmz.atlassian.net/browse/AIS-15972

--- Tests/Sources/BUs/RTSServicesTestCase.m
+++ Tests/Sources/BUs/RTSServicesTestCase.m
@@ -236,13 +236,13 @@ static NSString * const kInvalidMediaId = @"999999999999999";
     [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
-// Not supported for RTS
 - (void)testTVShowsWithUids
 {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Request succeeded"];
     
     [[self.dataProvider tvShowsWithUids:@[kTVShowUid, kTVShowOtherUid] completionBlock:^(NSArray<SRGShow *> * _Nullable shows, NSError * _Nullable error) {
-        XCTAssertNotNil(error);
+        XCTAssertNotNil(shows);
+        XCTAssertNil(error);
         [expectation fulfill];
     }] resume];
     
@@ -434,13 +434,13 @@ static NSString * const kInvalidMediaId = @"999999999999999";
     [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
-// Not supported for RTS
 - (void)testRadioShowsWithUids
 {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Request succeeded"];
     
     [[self.dataProvider radioShowsWithUids:@[kRadioShowUid, kRadioShowOtherUid] completionBlock:^(NSArray<SRGShow *> * _Nullable shows, NSError * _Nullable error) {
-        XCTAssertNotNil(error);
+        XCTAssertNotNil(shows);
+        XCTAssertNil(error);
         [expectation fulfill];
     }] resume];
     
