b3b1fc59ffe28b61c8e5061fdeeb07c6f7005491
Samuel Défago <defagos@gmail.com>
2017-07-14 21:00:36 +0200

    Prepare tests

---
+++ Tests/Sources/SubdivisionTestCase.m
@@ -0,0 +1,41 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import "DataProviderBaseTestCase.h"
+
+@interface SubdivisionTestCase : DataProviderBaseTestCase
+
+@end
+
+@implementation SubdivisionTestCase
+
+// Test:
+//   - Two disjoint subdivisions
+//   - Two disjoint blocked subdivisions
+//   - Two overlapping subdivisions
+
+//   - Subdivision overlapping with blocked one
+//   - Blocked subdivion overlapping with normal one
+//   - Two overlapping subdivisions
+
+//   - Two nested normal subdivisions
+//   - Blocked subdivision nested in non-blocked one
+//   - Non-blocked subdivision nested in blocked one
+//   - Twpo nested blocked subdivisions
+//   - Two overlapping suvdivisions with same markin, different durations
+//   - Two overlapping suvdivisions with same markout, different durations
+//   - Two identical subdivisions
+
+//   - SRF use cases
+
+//   - Empty subdivisions
+
+- (void)testDisjointSubdivisions
+{
+    
+}
+
+@end
