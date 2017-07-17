cfd4b02e77ebdec106b633f541057e4b584f559f
Samuel Défago <defagos@gmail.com>
2017-07-17 08:57:22 +0200

    Add tests for nested subdivisions

--- Tests/Sources/ChapterTestCase.m
+++ Tests/Sources/ChapterTestCase.m
@@ -24,7 +24,7 @@
 //   - Two nested normal subdivisions
 //   - Blocked subdivision nested in non-blocked one
 //   - Non-blocked subdivision nested in blocked one
-//   - Twpo nested blocked subdivisions
+//   - Two nested blocked subdivisions
 //   - Two overlapping suvdivisions with same markin, different durations
 //   - Two overlapping suvdivisions with same markout, different durations
 //   - Two identical subdivisions
@@ -279,6 +279,103 @@
     XCTAssertEqual(segment1.markOut, 35);
     XCTAssertEqual(segment1.duration, 20);
     XCTAssertEqual(segment1.blockingReason, SRGBlockingReasonGeoblocking);
+}
+
+- (void)testNestedNormalSubdivisions
+{
+    NSError *error = nil;
+    NSDictionary *JSONDictionary = @{ @"segmentList" : @[
+                                              @{ @"title" : @"A",
+                                                 @"position" : @0,
+                                                 @"markIn" : @10,
+                                                 @"markOut" : @60,
+                                                 @"duration" : @50 },
+                                              
+                                              @{ @"title" : @"B",
+                                                 @"position" : @1,
+                                                 @"markIn" : @20,
+                                                 @"markOut" : @40,
+                                                 @"duration" : @20 }
+                                              ] };
+    
+    SRGChapter *chapter = [MTLJSONAdapter modelOfClass:[SRGChapter class] fromJSONDictionary:JSONDictionary error:&error];
+    XCTAssertNil(error);
+    
+    NSArray<SRGSegment *> *segments = chapter.segments;
+    XCTAssertEqual(segments.count, 3);
+    
+    SRGSubdivision *segment0 = segments[0];
+    XCTAssertEqual(segment0.title, @"A");
+    XCTAssertEqual(segment0.position, 0);
+    XCTAssertEqual(segment0.markIn, 10);
+    XCTAssertEqual(segment0.markOut, 60);
+    XCTAssertEqual(segment0.duration, 50);
+    XCTAssertEqual(segment0.blockingReason, SRGBlockingReasonNone);
+    
+    SRGSubdivision *segment1 = segments[1];
+    XCTAssertEqual(segment1.title, @"B");
+    XCTAssertEqual(segment1.position, 0);
+    XCTAssertEqual(segment1.markIn, 10);
+    XCTAssertEqual(segment1.markOut, 60);
+    XCTAssertEqual(segment1.duration, 50);
+    XCTAssertEqual(segment1.blockingReason, SRGBlockingReasonNone);
+    
+    SRGSubdivision *segment2 = segments[2];
+    XCTAssertEqual(segment2.title, @"A");
+    XCTAssertEqual(segment2.position, 0);
+    XCTAssertEqual(segment2.markIn, 10);
+    XCTAssertEqual(segment2.markOut, 60);
+    XCTAssertEqual(segment2.duration, 50);
+    XCTAssertEqual(segment2.blockingReason, SRGBlockingReasonNone);
+}
+
+- (void)testNestedBlockedInNormalSubdivisions
+{
+    NSError *error = nil;
+    NSDictionary *JSONDictionary = @{ @"segmentList" : @[
+                                              @{ @"title" : @"A",
+                                                 @"position" : @0,
+                                                 @"markIn" : @10,
+                                                 @"markOut" : @60,
+                                                 @"duration" : @50 },
+                                              
+                                              @{ @"title" : @"B",
+                                                 @"position" : @1,
+                                                 @"markIn" : @20,
+                                                 @"markOut" : @40,
+                                                 @"duration" : @20,
+                                                 @"blockReason" : @"GEOBLOCK" }
+                                              ] };
+    
+    SRGChapter *chapter = [MTLJSONAdapter modelOfClass:[SRGChapter class] fromJSONDictionary:JSONDictionary error:&error];
+    XCTAssertNil(error);
+    
+    NSArray<SRGSegment *> *segments = chapter.segments;
+    XCTAssertEqual(segments.count, 3);
+    
+    SRGSubdivision *segment0 = segments[0];
+    XCTAssertEqual(segment0.title, @"A");
+    XCTAssertEqual(segment0.position, 0);
+    XCTAssertEqual(segment0.markIn, 10);
+    XCTAssertEqual(segment0.markOut, 60);
+    XCTAssertEqual(segment0.duration, 50);
+    XCTAssertEqual(segment0.blockingReason, SRGBlockingReasonNone);
+    
+    SRGSubdivision *segment1 = segments[1];
+    XCTAssertEqual(segment1.title, @"B");
+    XCTAssertEqual(segment1.position, 0);
+    XCTAssertEqual(segment1.markIn, 10);
+    XCTAssertEqual(segment1.markOut, 60);
+    XCTAssertEqual(segment1.duration, 50);
+    XCTAssertEqual(segment1.blockingReason, SRGBlockingReasonGeoblocking);
+    
+    SRGSubdivision *segment2 = segments[2];
+    XCTAssertEqual(segment2.title, @"A");
+    XCTAssertEqual(segment2.position, 0);
+    XCTAssertEqual(segment2.markIn, 10);
+    XCTAssertEqual(segment2.markOut, 60);
+    XCTAssertEqual(segment2.duration, 50);
+    XCTAssertEqual(segment2.blockingReason, SRGBlockingReasonNone);
 }
 
 @end
