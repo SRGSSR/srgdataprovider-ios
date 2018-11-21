9141d4b5a51c77e503cfc5579ba6f695b439db7f
Samuel Défago <defagos@gmail.com>
2018-11-21 14:16:49 +0100

    Add failing test with larger page size than the default

--- Tests/Sources/RequestTestCase.m
+++ Tests/Sources/RequestTestCase.m
@@ -578,7 +578,7 @@
     [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
-- (void)testURNFirstPageOverride
+- (void)testURNSmallerFirstPageOverride
 {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Request succeeded"];
     
@@ -594,6 +594,26 @@
     
     XCTAssertEqual(request.page.number, 0);
     XCTAssertEqual(request.page.size, 4);
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
+}
+
+- (void)testURNLargerFirstPageOverride
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Request succeeded"];
+    
+    NSArray<NSString *> *URNs = @[@"urn:rts:video:10002568", @"urn:rts:video:10002444", @"urn:rts:video:9986412", @"urn:rts:video:9986195",
+                                  @"urn:rts:video:9948638", @"urn:rts:video:9951674", @"urn:rts:video:9951724", @"urn:rts:video:9950129",
+                                  @"urn:rts:video:9949270", @"urn:rts:video:9948800", @"urn:rts:video:9948698", @"urn:rts:video:9946068",
+                                  @"urn:rts:video:9946141"];
+    SRGPageRequest *request = [[[self.dataProvider mediasWithURNs:URNs completionBlock:^(NSArray<SRGMedia *> * _Nullable medias, SRGPage *page, SRGPage * _Nullable nextPage, NSHTTPURLResponse * _Nullable HTTPResponse, NSError * _Nullable error) {
+        XCTAssertEqual(medias.count, 12);
+        [expectation fulfill];
+    }] requestWithPageSize:12] requestWithPage:nil];
+    [request resume];
+    
+    XCTAssertEqual(request.page.number, 0);
+    XCTAssertEqual(request.page.size, 12);
     
     [self waitForExpectationsWithTimeout:30. handler:nil];
 }
