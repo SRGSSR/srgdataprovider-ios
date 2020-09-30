7f56fada2724e70a782f5f8a9ef51aa4f0413538
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-09-30 18:28:53 +0200

    Update test case name

--- Tests/SRGDataProviderCombineTests/DataProviderCombineTestCase.swift
+++ Tests/SRGDataProviderCombineTests/DataProviderCombineTestCase.swift
@@ -12,7 +12,7 @@ enum TestError: Error {
 }
 
 @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-final class SRGDataProviderCombineTests: XCTestCase {
+final class DataProviderCombineTestCase: XCTestCase {
     var dataProvider: SRGDataProvider!
     var cancellables: Set<AnyCancellable>!
     
