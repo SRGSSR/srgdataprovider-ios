c90f0169ec7f355326f674792e3ee01c04322256
Samuel Défago <defagos@gmail.com>
2021-02-12 13:18:20 +0100

    Bump version number

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 struct ProjectSettings {
-    static let marketingVersion: String = "10.0.0"
+    static let marketingVersion: String = "10.0.1"
 }
 
 let package = Package(
